e804f1c308fd19eff8268383d7f47a4dcbbf6dc1
Vindi <kelvinandak@gmail.com>
2024-07-03 20:21:07 +0100

    finished demo project

--- carousel.js
+++ carousel.js
@@ -1,9 +1,30 @@
-const slider = document.querySelector('.slider');
+const slider = document.querySelector(".slider");
 
 function activate(e) {
-  const items = document.querySelectorAll('.item');
-  e.target.matches('.next') && slider.append(items[0])
-  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+  const items = document.querySelectorAll(".item");
+  e.target.matches(".next") && slider.append(items[0]);
+  e.target.matches(".prev") && slider.prepend(items[items.length - 1]);
 }
 
-document.addEventListener('click',activate,false);
+document.addEventListener("click", activate, false);
+
+// changing slide images after every 10 seconds
+function changeSlideImage() {
+  const nextBtn = document.querySelector(".next");
+  const prevBtn = document.querySelector(".prev");
+  // setting an interval for next button
+  let nextInterval = setInterval(() => {
+    nextBtn.click();
+    nextInterval;
+  }, 5000);
+  prevBtn.addEventListener("click", () => {
+    clearInterval(nextInterval);
+  });
+  if (nextBtn) {
+    nextInterval;
+  }
+
+  // clearing the interval for next button if the prev button is clicked
+}
+
+changeSlideImage();
